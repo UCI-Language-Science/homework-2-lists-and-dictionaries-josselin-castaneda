1b8c060a97e6e9523070f22d60ad05eb6f4929fc
josselin-castaneda <josselgc@uci.edu>
2025-01-24 21:06:53 +0000

    finished duplicate_remover

--- duplicate_remover.py
+++ duplicate_remover.py
@@ -7,8 +7,12 @@
 # 
 def duplicate_remover():
     duplicates_list = [1, 4, 3, 4, 2, 5, 1, 2, 7, 9, 4]
-    
-    
+    non_duplicate = []
+
+    for number in duplicates_list:
+        if number not in non_duplicate:
+            non_duplicate = non_duplicate + [number]
+    print(non_duplicate)
 
 if __name__ == "__main__":
     duplicate_remover()
